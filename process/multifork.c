b6b1c2344f9bb5096db188e4ee0f6961a4f1173e
ReyRen <reyren179@gmail.com>
2017-10-28 21:43:00 -0400

    process supplement for system, wait, multifork

--- process/multifork.c
+++ process/multifork.c
@@ -14,6 +14,7 @@ void LoopFunc(int num)
 int main(void)
 {
 	pid_t pid;
+	int ret;
 	int procNum = 0;
 	int loopNum = 0;
 	int i,j;
@@ -43,6 +44,19 @@ int main(void)
 			return 0;//让子进程跑圈，子进程不参与下一次的fork
         	}
 	}
+	//检测到所有子进程退出，父进程才退出
+	while(1)
+	{
+		ret = wait(NULL);//让父进程阻塞，阻塞过程中有可能被别的信号中断...需要再做异常处理
+		if(ret == -1)
+		{
+			if(error == EINTR)
+			{
+				continue;
+			}
+			break;
+		}
+	}	
 	return 0;
 }
 /*
